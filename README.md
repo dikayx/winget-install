52cb0fbfadeaeb71cf4c3303b1f5a3d421da8c6f
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2021-09-24 10:56:28 +0200

    Added installation script V1

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# winget-install
-Installation script for my personal apps on a windows system
+# Automatic installation script for Windows
+
+Installation script for my personal apps on a windows system. Feel free to use and modify it depending on your needs.
+
+## Requirements
+
+- OS: Windows 10 (or above)
+- `winget` must be installed. Download msixbundle [here](https://github.com/microsoft/winget-cli/releases)
+
+### How to run
+
+- Open a terminal with administrator privileges
+- Navigate to `install.bat` file location
+- Run the file
+- Wait\* :)
+
+_\*You may need to accept some UAC pop-ups on individual installation processes._
---
+++ install.bat
@@ -0,0 +1,32 @@
+:: Automatic installation for my personal apps on Windows systems
+:: Created by Dan Koller 2021
+
+echo "Installing essential software..."
+:: Install basic programs
+winget install Google.Chrome
+winget install Google.DriveFileStream
+winget install Microsoft.WindowsTerminal
+winget install Notepad++.Notepad++
+winget install Discord.Discord
+echo "...done!"
+
+echo "Installing development tools..."
+:: Install development programs
+:: Runtimes
+winget install AdoptOpenJDK.OpenJDK -v 11.0.7
+winget install Python.Python
+winget install OpenJS.Nodejs
+:: IDEs & editors
+winget install JetBrains.IntelliJIDEA.Community
+winget install JetBrains.PyCharm.Community
+winget install Microsoft.VisualStudioCode
+:: Other tools
+winget install GitHub.GitHubDesktop
+winget install Postman.Postman
+winget install wsl --install
+echo "...done!"
+
+:: Other tools
+echo "Install Git manually (https://git-scm.com/downloads)."
+echo "Don't forget to install gradle (https://gradle.org/install/)"
+echo "Copy terminal settings from https://gist.github.com/dan-koller/e8eefc0d0c987f5a57666d28e6ba8cec"
