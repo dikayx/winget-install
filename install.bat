d07ef3e201ae6684862189c194c64fce73be873b
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2023-01-08 15:34:25 +0100

    Updated packages and installation process

--- install.bat
+++ install.bat
@@ -7,6 +7,11 @@ winget install -e --id Google.Chrome
 winget install -e --id Google.Drive
 echo "...done!"
 
+echo "Installing utilities..."
+:: Install utility programs
+winget install -e --id smartmontools.smartmontools
+echo "...done!"
+
 echo "Installing development tools..."
 :: Install development programs
 winget install -e --id JetBrains.IntelliJIDEA.Community
@@ -20,14 +25,18 @@ winget install -e --id GitHub.cli
 echo "...done!"
 
 echo "Configuring additional settings..."
-:: Configure git
-git config --global core.autocrlf true
 :: Copy settings
-xcopy .\settings.json C:\Users\Daniel\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
+xcopy .\settings.json %localappdata%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
+xcopy .\gitconfig %userprofile%\.gitconfig
 
 :: Other tools
 echo "MSSQL Server can be downloaded from https://www.microsoft.com/en-us/sql-server/sql-server-downloads"
 echo "SQL Server Management Studio can be downloaded from https://docs.microsoft.com/en-us/sql/ssms/download-sql-server-management-studio-ssms?view=sql-server-ver18"
 
 :: Reminders
+echo "Please configure your git user and email:"
+echo "git config --global user.name '<your name>'"
+echo "git config --global user.email '<your email>'"
+
+:: Finish
 echo "Installation finished. Please restart your system now."
