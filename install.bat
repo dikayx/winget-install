da9d1f0dc4c1559b9126bf5b094e2542580922b1
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2023-07-12 15:50:01 +0200

    Updated software packages

--- install.bat
+++ install.bat
@@ -5,6 +5,7 @@ echo "Installing essential software..."
 :: Install basic programs
 winget install -e --id Google.Chrome
 winget install -e --id Google.Drive
+winget install -e --id Discord.Discord
 echo "...done!"
 
 echo "Installing utilities..."
@@ -15,9 +16,9 @@ echo "...done!"
 echo "Installing development tools..."
 :: Install development programs
 winget install -e --id JetBrains.IntelliJIDEA.Community
-winget install -e --id JetBrains.PyCharm.Community
 winget install -e --id Microsoft.VisualStudio.2022.Community
 winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Microsoft.OpenJDK.17
 winget install -e --id OpenJS.NodeJS.LTS
 winget install -e --id Postman.Postman
 winget install -e --id Git.Git
@@ -26,7 +27,6 @@ echo "...done!"
 
 echo "Configuring additional settings..."
 :: Copy settings
-xcopy .\settings.json %localappdata%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
 xcopy .\gitconfig %userprofile%\.gitconfig
 
 :: Other tools
