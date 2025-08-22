7b0ed5511f192ad18795c5a44d8e3c4047d8afb4
Dan Koller <57103678+dikayx@users.noreply.github.com>
2025-08-22 17:09:44 +0200

    Updated tweaks since there is no reliable way to set the default browser via registry

--- Scripts/Tweaks.ps1
+++ Scripts/Tweaks.ps1
@@ -2,21 +2,11 @@
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0 -Type DWord
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 0 -Type DWord
 
-# Disable hiding file extensions
+# Explorer tweaks
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Value 0 -Type DWord
-
-# Show hidden files and folders
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Hidden" -Value 1 -Type DWord
-
-# Vollständige Pfade in der Adressleiste anzeigen
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "FullPathAddressBar" -Value 1 -Type DWord
-
-# Vorherige Ordnerfenster bei der Anmeldung wiederherstellen
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\SessionInfo" -Name "RestorePreviousFolder" -Value 1 -Type DWord
-
-# Set the default browser to Google Chrome
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice" -Name "ProgId" -Value "ChromeHTML" -Type String
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice" -Name "ProgId" -Value "ChromeHTML" -Type String
 
 # Restart Explorer to apply changes
 Stop-Process -Name explorer -Force
